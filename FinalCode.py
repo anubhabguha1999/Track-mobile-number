d7fafbed0042b944926af28895583ba33d3e812b
Anubhab Guha <96384072+anubhabguha1999@users.noreply.github.com>
2023-02-07 01:24:05 +0530

    Add files via upload

---
+++ FinalCode.py
@@ -0,0 +1,12 @@
+import phonenumbers
+from phonenumbers import timezone,geocoder,carrier
+
+number1 = input("Enter Your number with (+91): ")
+phone = phonenumbers.parse(number1)
+time=timezone.time_zones_for_number(phone)
+car= carrier.name_for_number(phone, "en")
+reg = geocoder.description_for_number(phone, "en")
+print(phone)
+print(time)
+print(car)
+print(reg) 
